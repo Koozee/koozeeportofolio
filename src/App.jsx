883086631a6438978aca00b07d8075eacc40c722
Koozee <nicoachmad15@gmail.com>
2024-08-08 18:26:55 +0700

    completing section

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,19 @@ import TittleContent from "./components/Elements/TittleContent";
 function App() {
   return (
     <>
-      <main className="flex h-screen bg-gradient-to-tr from-white via-white to-sky-300">
+      <main className="flex h-screen bg-gradient-to-tr from-white via-white to-sky-300 px-5">
         {/* nanti akan diisi form guest saat diawal pages */}
         <div className="container m-auto flex flex-col justify-center items-center w-max gap-y-5">
-          <h1 className="text-4xl lg:text-5xl text-purple-600 font-bold">Hello Guest,</h1>
+          <h1 className="text-4xl lg:text-5xl  text-purple-600 font-bold">Hello Guest,</h1>
           <h2 className="text-4xl lg:text-6xl text-purple-600 font-semibold">I am Niko Achmad</h2>
-          <div className="flex lg:gap-x-5">
-            <div className="relative">
+          <div className="flex gap-x-5 xl:gap-x-6 justify-center">
+            <div className="relative w-[40%] lg:w-full">
               <h3 className="z-50 absolute text-4xl lg:text-6xl font-bold text-blue-600">Frontend</h3>
               <div className="z-0 text-transparent top-0 w-full h-[110%] transform -translate-x-2 translate-y-2 -rotate-6 rounded-lg bg-gradient-to-r from-[#5fbdff]/50 to-[#e5f4ff]/50">--------------------------------------</div>
             </div>
-            <h3 className="text-4xl lg:text-6xl font-bold text-blue-500">Developer</h3>
+            <h3 className="text-4xl lg:text-6xl  font-bold text-blue-500">Developer</h3>
           </div>
-          <p className="font-medium text-slate-600 text-2xl lg:text-3xl text-center w-[90%] lg:w-[60%] mt-5">Information engineering student from Malang who has an interest in web development</p>
+          <p className="font-medium text-slate-600 text-2xl lg:text-3xl text-center lg:w-[60%] mt-5">Information engineering student from Malang who has an interest in web development</p>
           <div className="mt-10 grid grid-cols-3 gap-8 lg:gap-16">
             <Buttoncontact linkgoto={"mailto:nicoachmad15@gmail.com"} logo={image.logogmail} />
             <Buttoncontact linkgoto={"https://github.com/Koozee"} logo={image.logogithub} />
@@ -26,20 +26,20 @@ function App() {
         </div>
       </main>
 
-      <article className="2xl:px-72 xl:px-48 px-5 my-20">
-        <TittleContent tittletext={"😎About Me"} />
-        <div className="container flex flex-col xl:flex-row justify-between gap-3">
+      <article className="2xl:px-72 xl:px-48 px-5">
+        <TittleContent variant={"text-sky-600"} tittletext={"😎About Me"} />
+        <div className="container flex flex-col-reverse xl:flex-row justify-between gap-3">
           <div className="xl:w-2/3 xl:pr-20 text-justify">
-            <p className="text-gray-950 font-light text-xl xl:text-2xl">Hello, I'm Niko Achmad, a front end developer from Malang. I am a bachelor's degree student in Informatics Engineering from PGRI Kanjuruhan University Malang.</p>
-            <p className="text-gray-950 font-light text-xl xl:text-2xl py-5">With a strong background in HTML, CSS, and JavaScript, I enjoy creating engaging and functional digital experiences.
-              Apart from coding, I enjoy exploring technology, playing games, and exploring nature.</p>
+            <p className="text-gray-950 font-light text-xl xl:text-3xl">Hi, I'm Niko Achmad, a front end developer from Malang. I am a Bachelor of Informatics Engineering student from Universitas PGRI Kanjuruhan Malang. I am very interested in the world of programming, especially in web development, my favorite programming language is javascript because it is easy to learn, modern, and has a large community. I am not only a coder but also creative, problem solving and can adapt to new things.</p>
+            <p className="text-gray-950 font-light text-xl xl:text-3xl py-5">With a strong background in web fundamentals I enjoy creating engaging and functional digital experiences. Besides coding, I enjoy exploring technology, playing games, and exploring nature.</p>
+            
           </div>
-          <img className="xl:w-1/3" src="https://i.pinimg.com/564x/e5/c9/bb/e5c9bb4575148d1284411f852429cf80.jpg" alt="" />
+          <img className="xl:w-1/3" src={image.akudewe} alt="" />
         </div>
       </article>
 
       <section className="2xl:px-72 xl:px-48 px-5">
-        <TittleContent tittletext={"🚀Amazing Project"} />
+        <TittleContent variant={"text-sky-600"} tittletext={"🚀Amazing Project"} />
         <h3 className="text-gray-950 text-xl xl:text-2xl text-justify xl:text-center">As a frontend web developer, I have worked on various projects that showcase my skills and creativity. Explore my work and see how I turn ideas into amazing web experiences.</h3>
         <div className="container mt-16">
           <div className="xl:grid grid-cols-2 gap-10">
@@ -87,40 +87,40 @@ function App() {
         </div>
       </section>
 
-      <section className="mt-20 bg-slate-800/90">
-          <TittleContent tittletext={"🤖Skills"} />
+      <section className="2xl:px-72 xl:px-48 px-5 mt-20 bg-slate-800/90">
+        <TittleContent variant={"text-sky-200"} tittletext={"📚Skills"} />
         <p className="text-white text-center text-2xl">As a Frontend Web Developer I have several technologies and tools to create engaging and responsive experiences</p>
-        <div className="container mx-auto w-max grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 py-32">
-          <div className="relative w-max p-4 bg-slate-200/75 rounded-lg ring-2 ring-orange-300 shadow-lg shadow-orange-500/80 animate-border-orange">
-            <img src={image.htmllogo} alt="Card Image" />
+        <div className="container mx-auto w-full grid grid-cols-2 lg:grid-cols-4 gap-5 lg:gap-20 py-16 lg:py-32">
+          <div className="relative mx-auto w-[80%] lg:w-max p-4 bg-slate-200/75 rounded-lg ring-2 ring-orange-300 shadow-lg shadow-orange-500/80 animate-border-orange">
+            <img className="m-auto" src={image.htmllogo} alt="Card Image" />
           </div>
-          <div className="relative w-max p-4 bg-slate-200/75 rounded-lg ring-2 ring-blue-300 shadow-lg shadow-blue-500/80 animate-border-blue">
-            <img src={image.csslogo} alt="Card Image" />
+          <div className="relative mx-auto w-[80%] lg:w-max p-4 bg-slate-200/75 rounded-lg ring-2 ring-blue-300 shadow-lg shadow-blue-500/80 animate-border-blue">
+            <img className="m-auto" src={image.csslogo} alt="Card Image" />
           </div>
-          <div className="relative w-max p-4 bg-slate-200/75 rounded-lg ring-2 ring-yellow-300 shadow-lg shadow-yellow-500/80 animate-border-yellow">
-            <img src={image.jslogo} alt="Card Image" />
+          <div className="relative mx-auto w-[80%] lg:w-max p-4 bg-slate-200/75 rounded-lg ring-2 ring-yellow-300 shadow-lg shadow-yellow-500/80 animate-border-yellow">
+            <img className="m-auto" src={image.jslogo} alt="Card Image" />
           </div>
-          <div className="relative w-max p-4 bg-slate-200/75 rounded-lg ring-2 ring-blue-300 shadow-lg shadow-blue-500/80 animate-border-blue">
-            <img src={image.javalogo} alt="Card Image" />
+          <div className="relative mx-auto w-[80%] lg:w-max p-4 bg-slate-200/75 rounded-lg ring-2 ring-blue-300 shadow-lg shadow-blue-500/80 animate-border-blue">
+            <img className="m-auto" src={image.javalogo} alt="Card Image" />
           </div>
-          <div className="relative w-max p-4 bg-slate-200/75 rounded-lg ring-2 ring-blue-300 shadow-lg shadow-blue-500/80 animate-border-blue">
-            <img src={image.tailwindlogo} alt="Card Image" />
+          <div className="relative mx-auto w-[80%] lg:w-max p-4 bg-slate-200/75 rounded-lg ring-2 ring-blue-300 shadow-lg shadow-blue-500/80 animate-border-blue">
+            <img className="m-auto" src={image.tailwindlogo} alt="Card Image" />
           </div>
-          <div className="relative w-max p-4 bg-slate-200/75 rounded-lg ring-2 ring-purple-300 shadow-lg shadow-purple-500/80 animate-border-purple">
-            <img src={image.bootstarplogo} alt="Card Image" />
+          <div className="relative mx-auto w-[80%] lg:w-max p-4 bg-slate-200/75 rounded-lg ring-2 ring-purple-300 shadow-lg shadow-purple-500/80 animate-border-purple">
+            <img className="m-auto" src={image.bootstarplogo} alt="Card Image" />
           </div>
-          <div className="relative w-max p-4 bg-slate-200/75 rounded-lg ring-2 ring-blue-300 shadow-lg shadow-blue-500/80 animate-border-blue">
-            <img src={image.reactlogo} alt="Card Image" />
+          <div className="relative mx-auto w-[80%] lg:w-max p-4 bg-slate-200/75 rounded-lg ring-2 ring-blue-300 shadow-lg shadow-blue-500/80 animate-border-blue">
+            <img className="m-auto" src={image.reactlogo} alt="Card Image" />
           </div>
-          <div className="relative w-max p-4 bg-slate-200/75 rounded-lg ring-2 ring-orange-300 shadow-lg shadow-orange-500/80 animate-border-orange">
-            <img src={image.laravellogo} alt="Card Image" />
+          <div className="relative mx-auto w-[80%] lg:w-max p-4 bg-slate-200/75 rounded-lg ring-2 ring-orange-300 shadow-lg shadow-orange-500/80 animate-border-orange">
+            <img className="m-auto" src={image.laravellogo} alt="Card Image" />
           </div>
         </div>
       </section>
 
-      <section className="2xl:px-72 xl:px-48">
-        <TittleContent tittletext={"💫My Journey"} />
-        <h3 className="text-gray-950 text-2xl">This is the journey and experience that I have carried out while being a front end web developer</h3>
+      <section className="2xl:px-72 xl:px-48 px-5">
+        <TittleContent variant={"text-sky-600"} tittletext={"💫My Journey"} />
+        <h3 className="text-gray-950 text-center lg:text-left text-2xl">This is the journey and experience that I have carried out while being a front end web developer</h3>
         <div className="container my-10">
           <div className="flex flex-col lg:flex-row">
             <div className="w-full md:w-1/2 py-5">
@@ -190,7 +190,7 @@ function App() {
                 Front End Developer
               </p>
               <p className="pt-2 text-start lg:text-lg">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cupiditate, suscipit consequatur officia minus labore rem ipsam voluptates magnam sed architecto, quae eos? Quibusdam repudiandae id pariatur, tenetur alias vel magni sed nemo harum dolor recusandae provident enim optio ipsum ab vitae. Quaerat dolorem nam perferendis blanditiis enim eaque, placeat harum sunt repellendus delectus! Velit placeat dolore debitis nam, minus, aperiam quisquam earum harum maxime id animi obcaecati dolor repellat dignissimos mollitia tenetur voluptas? Dicta quisquam possimus porro sapiente quis dolore eum quaerat totam, temporibus officia suscipit quidem assumenda? Nisi facere, at veritatis quas autem eius amet quod maiores recusandae!
+                I took part in the IMRO 2024 competition at STKIP Modern Ngawi in the general IoT category as a Front End Web Developer. My task in the team is to create a web application interface for our team's IoT product, namely smart mirror, the framework that our team uses for web application development is Laravel because it is easy and fast to develop.
               </p>
             </div>
           </div>
@@ -224,21 +224,61 @@ function App() {
                 Universitas Widya Husada Semarang
               </p>
               <p className="pt-2 text-start lg:text-lg">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cupiditate, suscipit consequatur officia minus labore rem ipsam voluptates magnam sed architecto, quae eos? Quibusdam repudiandae id pariatur, tenetur alias vel magni sed nemo harum dolor recusandae provident enim optio ipsum ab vitae. Quaerat dolorem nam perferendis blanditiis enim eaque, placeat harum sunt repellendus delectus! Velit placeat dolore debitis nam, minus, aperiam quisquam earum harum maxime id animi obcaecati dolor repellat dignissimos mollitia tenetur voluptas? Dicta quisquam possimus porro sapiente quis dolore eum quaerat totam, temporibus officia suscipit quidem assumenda? Nisi facere, at veritatis quas autem eius amet quod maiores recusandae!
+                I created a landing page web design using HTML, TailwindCSS and JavaScript technology with the theme "Entrepreneurship and Health" and I gave the web title the name HealthTech. This is my first project as a front end web developer and I am trying to submit it to a competition at Widya Husada University Semarang for Indonesia's 78th anniversary.
               </p>
             </div>
           </div>
         </div>
       </section>
 
-      {/* <section className="2xl:px-72 xl:px-48">
-        <TittleContent tittletext={"Contact Me"} />
-        <div className="container">
+      <section className="2xl:px-72 xl:px-48 px-5">
+        <TittleContent variant={"text-sky-600"} tittletext={"📲Contact Me"} />
+        <div className="container lg:flex justify-between">
+          <div>
+            <img src="https://i.pinimg.com/564x/d4/71/1b/d4711b79e448df376ccbc6281fcce489.jpg" alt="" />
+          </div>
+          <div className="lg:w-1/2">
+            <h4 className="text-gray-950 text-center py-5 text-lg lg:text-2xl">Have a question or project you'd like to discuss? Send me a message and I'll get back to you soon!😁</h4>
+            <form className="relative lg:translate-y-1/4 flex flex-col gap-5" action="">
+              <input className="border-2 rounded-lg pl-3 py-2 w-full " type="text" name="guestname" id="" placeholder="Your Name" required />
+              <input className="border-2 rounded-lg pl-3 py-2 w-full " type="email" name="guestname" id="" placeholder="Your Email" required />
+              <input className="border-2 rounded-lg pl-3 py-2 w-full " type="text" name="guestname" id="" placeholder="Subjects" required />
+              <textarea className="border-2 rounded-lg pl-3 py-2 w-full" name="" id="" cols="30" placeholder="Your Messages" required></textarea>
 
+              <button className="group flex justify-center items-center gap-3 border-2 rounded-lg py-2 bg-gradient-to-r from-sky-500 via-sky-600 to-sky-500 
+              hover:bg-gradient-to-r hover:from-sky-400 hover:via-sky-500 hover:to-sky-400 transition-colors duration-300 ease-in-out">
+                <div className="text-slate-50 font-semibold text-lg">Send Message</div>
+                <svg
+                  width={24}
+                  height={24}
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6 relative group-hover:translate-x-3 transition-all duration-300 ease-in-out"
+                  preserveAspectRatio="xMidYMid meet"
+                >
+                  <g clipPath="url(#clip0_58_2)">
+                    <path
+                      d="M1.76 21.0004L1.76054 21.379L2.10852 21.2298L23.0985 12.2298L23.6344 12L23.0985 11.7702L2.10852 2.77023L1.76054 2.62103L1.76 2.99964L1.75 9.99964L1.74969 10.2188L1.96696 10.2478L15.1084 12L1.96696 13.7522L1.74969 13.7812L1.75 14.0004L1.76 21.0004Z"
+                      fill="#F8F8F8"
+                      stroke="black"
+                      strokeWidth="0.5"
+                    />
+                  </g>
+                  <defs>
+                    <clippath id="clip0_58_2">
+                      <rect width={24} height={24} fill="white" />
+                    </clippath>
+                  </defs>
+                </svg>
+              </button>
+
+            </form>
+          </div>
         </div>
-      </section> */}
+      </section>
 
-      <footer className="2xl:px-72 xl:px-48 bg-slate-800/95">
+      <footer className="2xl:px-72 xl:px-48 bg-slate-800/90 mt-20">
         <div className="container py-10">
           <p className="text-slate-50 text-center font-semibold text-4xl">Koozee</p>
           <p className="text-slate-50 text-center font-semibold text-4xl py-5">Follow Me</p>
@@ -253,9 +293,12 @@ function App() {
             <a href="" className="text-slate-200/80 hover:text-slate-50">
               <ion-icon size="large" name="logo-github"></ion-icon>
             </a>
+            <a href="" className="text-slate-200/80 hover:text-slate-50">
+              <ion-icon size="large" name="logo-youtube"></ion-icon>
+            </a>
           </div>
           <hr />
-          <p className="text-slate-50 font-semibold pt-4">© Niko Achmad 2024</p>
+          <p className="text-slate-50 font-semibold pt-4 text-lg pl-5">© Niko Achmad 2024</p>
         </div>
       </footer>
     </>
--- src/components/Elements/TittleContent.jsx
+++ src/components/Elements/TittleContent.jsx
@@ -1,9 +1,9 @@
 export default function Buttoncontact(props) {
     return (
-        <div className="my-20">
+        <div className="py-14 lg:py-20">
             <div className="flex justify-between">
                 <div className="relative">
-                    <h3 className="absolute z-50 w-max text-3xl xl:text-5xl font-bold text-sky-600">{props.tittletext}</h3>
+                    <h3 className={`absolute z-50 w-max text-3xl xl:text-5xl font-bold ${props.variant}`}>{props.tittletext}</h3>
                     <div className="z-0 w-full h-[150%] xl:h-[200%] text-transparent top-0 transform -translate-x-2 translate-y-2 -rotate-6 rounded-lg bg-gradient-to-r from-[#5fbdff]/50 to-[#e5f4ff]/50">-----------------------------------------------</div>
                 </div>
                 <div></div>
