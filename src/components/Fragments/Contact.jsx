095e69084e0f944ab8f148853e7540c8cbca6588
Koozee <nicoachmad15@gmail.com>
2025-02-02 17:43:09 +0700

    change CV Link

--- src/components/Fragments/Contact.jsx
+++ src/components/Fragments/Contact.jsx
@@ -12,7 +12,7 @@ export default function Contact(props) {
                         <p className="font-bold text-3xl">Koozee</p>
                         <p className="text-base">Frontend Developer</p>
                     </div>
-                    <a href="https://drive.google.com/file/d/1rQjamRf5maJ_HQ8EpYBR3Li1qnj1BPIR/view?usp=sharing" target="_blank" className="duration-300 hover:bg-sky-900 border hover:text-gray-50 bg-gray-50 font-semibold text-sky-800 px-3 py-2 flex flex-row items-center gap-3">Dowload CV
+                    <a href="https://drive.google.com/file/d/12YW7KEGZbroxvaCQBJNLlEl-LPMrkKpA/view?usp=sharing" target="_blank" className="duration-300 hover:bg-sky-900 border hover:text-gray-50 bg-gray-50 font-semibold text-sky-800 px-3 py-2 flex flex-row items-center gap-3">Dowload CV
                         <svg className="w-6 h-6 fill-current" height="100" preserveAspectRatio="xMidYMid meet" viewBox="0 0 100 100" width="100" x="0" xmlns="http://www.w3.org/2000/svg" y="0">
                             <path d="M22.1,77.9a4,4,0,0,1,4-4H73.9a4,4,0,0,1,0,8H26.1A4,4,0,0,1,22.1,77.9ZM35.2,47.2a4,4,0,0,1,5.7,0L46,52.3V22.1a4,4,0,1,1,8,0V52.3l5.1-5.1a4,4,0,0,1,5.7,0,4,4,0,0,1,0,5.6l-12,12a3.9,3.9,0,0,1-5.6,0l-12-12A4,4,0,0,1,35.2,47.2Z" fillRule="evenodd">
                             </path>
